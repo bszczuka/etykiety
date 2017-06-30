8235f6d5bcd166f8885184f4ad3a287e94382180
bogo <bszczuka@amsterdam-standard.pl>
2017-06-30 03:43:24 +0200

    Urls fix

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^labels$', views.label_list, name='label_list'),
-    url(r'^(?P<slug>[a-z, 0-9]+)/$', views.label, name='label'),
+    url(r'^(?P<slug>[a-z, 0-9, -]+)/$', views.label, name='label'),
 ]
